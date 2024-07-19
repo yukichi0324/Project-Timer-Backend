f10c1fd54adfc0d6d523089a312876590039395a
yukichi0324 <kajitani.code@gmail.com>
2024-07-19 20:26:05 +0900

    app.options 追加

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const cors = require('cors');
 const request = require('request');
 const app = express();
 
-// CORSミドルウェアを設定
+// CORSミドルウェアの設定
 app.use(cors({
-  origin: '*',
+  origin: '*', // 全てのオリジンを許可
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-cybozu-api-token']
 }));
 
-// リクエストボディをJSONとしてパース
 app.use(express.json());
 
+// プレフライトリクエストの明示的な処理
+app.options('/api/data', cors()); // プレフライトリクエストを明示的に処理
+
+// APIエンドポイントの設定
 app.post("/api/data", (req, res) => {
   const url = "https://jdg50gqzyue2.cybozu.com/k/v1/record.json";
   const options = {
@@ -28,7 +31,7 @@ app.post("/api/data", (req, res) => {
 });
 
 // サーバーを起動
-const port = 3000; // ポート番号を指定
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
