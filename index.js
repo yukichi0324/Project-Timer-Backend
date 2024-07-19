c232970a023950f38dc403c88ac600474e754511
yukichi0324 <kajitani.code@gmail.com>
2024-07-19 19:25:57 +0900

    cros 再修正

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const cors = require('cors');
 const request = require('request');
 const app = express();
 
-app.use(express.json());
-
+// CORSミドルウェアを設定
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-cybozu-api-token']
 }));
+
+// リクエストボディをJSONとしてパース
+app.use(express.json());
 
 app.post("/api/data", (req, res) => {
   const url = "https://jdg50gqzyue2.cybozu.com/k/v1/record.json";
@@ -25,6 +27,8 @@ app.post("/api/data", (req, res) => {
   request(options).pipe(res);
 });
 
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
+// サーバーを起動
+const port = 3000; // ポート番号を指定
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
