92945de0d4ae679bb019730ec35265524a389c38
yukichi0324 <kajitani.code@gmail.com>
2024-07-19 19:08:12 +0900

    CORS対策

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const express = require("express");
-const request = require("request");
+const express = require('express');
+const cors = require('cors');
+const request = require('request');
 const app = express();
-const port = 3001;
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-cybozu-api-token"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // POSTメソッドを許可
-  next();
-});
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-cybozu-api-token']
+}));
 
 app.post("/api/data", (req, res) => {
   const url = "https://jdg50gqzyue2.cybozu.com/k/v1/record.json";
